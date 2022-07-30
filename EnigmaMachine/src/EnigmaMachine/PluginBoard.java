d7ba6402b853c1f5ac63924fe3cdb6a4547137e9
chenberger <chen10.berger@gmail.com>
2022-07-30 12:12:17 +0300

    plugin board impl

--- EnigmaMachine/src/EnigmaMachine/PluginBoard.java
+++ EnigmaMachine/src/EnigmaMachine/PluginBoard.java
@@ -4,6 +4,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PluginBoard {
-    Map<Integer,Integer> plugedPairs = new HashMap<>();
-    PluginBoard()
+    private final Map<Character,Character> plugedPairs = new HashMap<>();
+    PluginBoard(String inputedPlugedPairs){
+        String[] pairs = inputedPlugedPairs.split(",");
+        for(String pair : pairs){
+            plugedPairs.put(pair.charAt(0),pair.charAt(1));
+            plugedPairs.put(pair.charAt(1),pair.charAt(0));
+        }
+    }
+    public Character getPlugedPair(Character inputedChar){
+        if(!plugedPairs.containsKey(inputedChar)){
+            return inputedChar;
+        }
+        return plugedPairs.get(inputedChar);
+    }
+
 }
