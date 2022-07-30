461aa419f3fb81b6b6586482aff11baa022324db
Erez Cohen <erez6356458@gmail.com>
2022-07-30 22:39:48 +0300

    Fix operation 2
    Signed-off-by: Erez Cohen <erez6356458@gmail.com>

--- EnigmaMachine/Engine/src/Operations/OperationsMachine.java
+++ EnigmaMachine/Engine/src/Operations/OperationsMachine.java
@@ -1,10 +1,12 @@
 package Operations;
 
+import TDO.MachineDetails;
+
 public interface OperationsMachine {
     public void automaticSettingsInitialize();
     public void manualSettingsInitialize();
     public void setMachineDetails();
-    public void viewMachineDetails();
+    public MachineDetails getMachineDetails() throws Exception;
     public void analyzeMachineHistoryAndStatistics();
     public void processInput();
 }
