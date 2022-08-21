7c59ec49da80f62286a28d168b8bc92ac063b2b3
chenberger <chen10.berger@gmail.com>
2022-08-21 18:12:32 +0300

    topBorderPane

--- EnigmaMachine/UserInterface/src/DesktopUserInterface/MainScene/TopScene/TopBorderPaneController.java
+++ EnigmaMachine/UserInterface/src/DesktopUserInterface/MainScene/TopScene/TopBorderPaneController.java
@@ -1,10 +1,12 @@
 package DesktopUserInterface.MainScene.TopScene;
 
 import DesktopUserInterface.MainScene.MainController;
+import Engine.EngineManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
 
 public class TopBorderPaneController {
     @FXML
@@ -14,9 +16,16 @@ public class TopBorderPaneController {
     @FXML
     private Label EnigmaMachineHeadlineTextField;
     private MainController mainController;
+    private EngineManager engineManager;
+
+    public TopBorderPaneController(){
+        engineManager = new EngineManager();
+    }
 
     @FXML
     void OnLoadMachineButtonClicked(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+
     }
 
     public void setMainController(MainController mainController) {
