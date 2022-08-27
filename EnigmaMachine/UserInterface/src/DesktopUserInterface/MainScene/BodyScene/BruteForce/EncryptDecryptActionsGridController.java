74cff8f4a591a8ac38f31144e4dedc94757b5fab
chenberger <chen10.berger@gmail.com>
2022-08-27 19:35:47 +0300

    prepared the brute force:

---
+++ EnigmaMachine/UserInterface/src/DesktopUserInterface/MainScene/BodyScene/BruteForce/EncryptDecryptActionsGridController.java
@@ -0,0 +1,8 @@
+package DesktopUserInterface.MainScene.BodyScene.BruteForce;
+
+public class EncryptDecryptActionsGridController {
+    private BruteForceGridController bruteForceGridController;
+    public void setBruteForceGridController(BruteForceGridController bruteForceGridController) {
+        this.bruteForceGridController = bruteForceGridController;
+    }
+}
